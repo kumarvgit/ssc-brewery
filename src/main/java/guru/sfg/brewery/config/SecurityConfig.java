da38cbf266c9786c015a7a2f45a4ab6adbe298ef
kumarvgit <vaibhav.cs.kumar@gmail.com>
2021-01-04 18:43:31 +0000

    Adding delegation faactory of password encoders

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,15 +1,39 @@
 package guru.sfg.brewery.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+//        Noop password encoder
+//        return NoOpPasswordEncoder.getInstance();
+
+//        LDAP password encoder
+//        return new LdapShaPasswordEncoder();
+
+//        SHA 256 encoder
+//        return new StandardPasswordEncoder();
+
+//          BCrypt encoder
+//        return new BCryptPasswordEncoder();
+
+//        using a factory to get all different types of algos
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -38,18 +62,46 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
         .withUser("spring")
                 // specify no password encoder in SpEL (spring Expression language)
-        .password("{noop}guru")
+
+        // use LDAP encoder
+//        .password("{SSHA}pzmyIka3tGtN0ptiuY/RtPaHKSfr+YHH+2PYyA==")
+
+        // SHA 256 encoder
+//        .password("5a5db2b9f1bd84a7ce46bd096ca9271f3819ecbaf61ce47cf8d20eaae7004ea80e196a18c8532912")
+
+        // BCrypt encoder
+        .password("{bcrypt}$2a$10$8FaWyk5vlogMrDZt1t8b3etFBcwJgWzDQQ1d9wAaZUunR75.gizIm")
         .roles("ADMIN")
 //                // using and is called Fluent API
+
         .and()
         .withUser("user")
-                // specify no password encoder in SpEL (spring Expression language)
-        .password("{noop}password")
+
+// specify no password encoder in SpEL (spring Expression language)
+//      .password("{noop}password")
+
+//      use bean to supply encoder
+//      LDAP encoder
+//      .password("{SSHA}GxAtMrccecHcPTAtFv20Eikx0vOUT0ItTU0hWg==")
+
+        // SHA 256 encoder
+        .password("{sha256}5a5db2b9f1bd84a7ce46bd096ca9271f3819ecbaf61ce47cf8d20eaae7004ea80e196a18c8532912")
+
+        // BCrypt encoder
+//        .password("$2a$10$4b9rjFaT7Kv6do0ANbDhwO4xCgsm307emQOahYqDU2X8Q5vK7NSgS")
         .roles("USER")
+
         .and()
         .withUser("scott")
-        .password("{noop}tiger")
-        .roles("CUSTOMER")
+        // using LDAP encoder
+        .password("{ldap}{SSHA}GW0AHJ3Wav8dDXOt9bx+6cUzyOFh3jjDwMX42g==")
+
+         // SHA 256 encoder
+//         .password("83f2e696f86dc7df0918b8b81d7ff581f0eda18c75a693b83d717d57b444aaa7e0b08946a7029535")
+
+          // BCrypt encoder
+//         .password("$2a$10$/8DKY6LrYQbZ.gLwpXl50OsKYuy9bMfWY1ybtlcghuEfQ0nsSZzbO")
+         .roles("CUSTOMER")
         ;
     }
 
