419fe9064a1a1d05ac2732dacb7d0d94ca04ce89
kumarvgit <vaibhav.cs.kumar@gmail.com>
2021-01-17 09:49:18 +0000

    Adding permission for beer order for admins and customers

--- src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
+++ src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
@@ -53,6 +53,21 @@ public class UserDataLoader implements CommandLineRunner {
         Authority updateBrewery = authorityRepository.save(Authority.builder().permission("brewery.update").build());
         Authority deleteBrewery = authorityRepository.save(Authority.builder().permission("brewery.delete").build());
 
+
+        // beer order permissions for admin, here in multitenancy this would be used to decide which role can do what operation
+        // in this case the admin can modify any order
+        Authority createOrder = authorityRepository.save(Authority.builder().permission("order.create").build());
+        Authority readOrder = authorityRepository.save(Authority.builder().permission("order.read").build());
+        Authority updateOrder = authorityRepository.save(Authority.builder().permission("order.update").build());
+        Authority deleteOrder = authorityRepository.save(Authority.builder().permission("order.delete").build());
+
+        // beer order permissions for customer, here in multitenancy this would be used to decide which role can do what operation
+        // in this case the customer can modify his order
+        Authority createOrderCustomer = authorityRepository.save(Authority.builder().permission("customer.order.create").build());
+        Authority readOrderCustomer = authorityRepository.save(Authority.builder().permission("customer.order.read").build());
+        Authority updateOrderCustomer = authorityRepository.save(Authority.builder().permission("customer.order.update").build());
+        Authority deleteOrderCustomer = authorityRepository.save(Authority.builder().permission("customer.order.delete").build());
+
         Role adminRole = roleRepository.save(Role.builder().name("ADMIN").build());
         Role customerRole = roleRepository.save(Role.builder().name("CUSTOMER").build());
         Role userRole = roleRepository.save(Role.builder().name("USER").build());
@@ -60,13 +75,17 @@ public class UserDataLoader implements CommandLineRunner {
         adminRole.setAuthorities(new HashSet<>(Set.of(
                 createBeer, readBeer, updateBeer, deleteBeer,
                 createBrewery, readBrewery, updateBrewery, deleteBrewery,
-                createCustomer, readCustomer, updateCustomer, deleteCustomer
+                createCustomer, readCustomer, updateCustomer, deleteCustomer,
+                createOrder, readOrder, updateOrder, deleteOrder
         )));
 
 //        This is going to throw UnsupportedOperationException
 //        customerRole.setAuthorities(Set.of(readBeer, readBrewery, readCustomer));
         // avoid UnsupportedOperationException since hibernate needs mutable instructions
-        customerRole.setAuthorities(new HashSet<>(Set.of(readBeer, readBrewery, readCustomer)));
+        customerRole.setAuthorities(new HashSet<>(Set.of(
+                readBeer, readBrewery, readCustomer,
+                createOrderCustomer, readOrderCustomer, updateOrderCustomer, deleteOrderCustomer
+        )));
 
         userRole.setAuthorities(new HashSet<>(Set.of(readBeer)));
 
