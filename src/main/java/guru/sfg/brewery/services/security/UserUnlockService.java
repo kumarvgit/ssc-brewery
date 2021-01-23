338e8a0bd46e712a475b0740bc765564f5b86ba7
kumarvgit <vaibhav.cs.kumar@gmail.com>
2021-01-23 19:14:02 +0000

    unlocking accounts

---
+++ src/main/java/guru/sfg/brewery/services/security/UserUnlockService.java
@@ -0,0 +1,38 @@
+package guru.sfg.brewery.services.security;
+
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserUnlockService {
+
+    private final UserRepository userRepository;
+
+    @Scheduled(fixedRate = 5000)
+    public void unlockAccounts(){
+
+        log.debug("Running unlock account");
+
+        List<User> allByAccountNonLockedAndLastModifiedDateIsBefore = userRepository.findAllByAccountNonLockedAndLastModifiedDateIsBefore(false,
+                Timestamp.valueOf(LocalDateTime.now().minusSeconds(30)));
+
+        if(!allByAccountNonLockedAndLastModifiedDateIsBefore.isEmpty()) {
+            log.debug("Locked account found");
+            allByAccountNonLockedAndLastModifiedDateIsBefore.forEach(p -> {
+                p.setAccountNonLocked(true);
+            });
+
+            userRepository.saveAll(allByAccountNonLockedAndLastModifiedDateIsBefore);
+        }
+    }
+}
